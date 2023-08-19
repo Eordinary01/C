4582648f3e31e566b94ac079d784f43df22b2127
Parth <parth.manocha2901@gmail.com>
2023-08-19 11:37:06 +0530

    Initial commit

---
+++ add.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a, b , c; 
+    printf("Enter number a\n");
+    scanf("%d", &a);
+    printf("Enter number b\n");
+    scanf("%d", &b);
+    // printf("The sum is %d\n", a+b);
+    printf("Enter number c\n");
+    scanf("%d", &c);
+    printf("The sum is %d\n", a+b+c);
+    return 0 ;
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello welcome to the C tutorial\n");
+    printf("In the free tutuorial and enjoy your class\n")  ;
+    return 0;
+}
