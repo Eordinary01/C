bddc1e1faa402d607d413a3a24438c0d8a08177d
Parth <parth.manocha2901@gmail.com>
2023-08-29 17:20:58 +0530

    Added fixes

--- test.c
+++ test.c
@@ -20,15 +20,32 @@
 
 
 
+// int main()
+// {
+//     int n ,i; 
+//     printf("Enter the number you want multiplication table of \n");
+//     scanf("%d", &n);
+//     for(  i =1; i <= 10; ++i){
+//         printf("%d * %d = %d \n" , n , i ,n*i);
+//     }
+//     return 0;
+
+
+// }
+
+
 int main()
 {
-    int n ,i; 
-    printf("Enter the number you want multiplication table of \n");
-    scanf("%d", &n);
-    for(  i =1; i <= 10; ++i){
-        printf("%d * %d = %d \n" , n , i ,n*i);
+    int i;
+
+    for( i=0; i<=100; i++){
+        if( i % 2 != 0){
+            printf("%d\n",i);
+
+        }
     }
+
     return 0;
-
-
 }
+
+
