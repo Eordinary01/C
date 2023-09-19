ce654416b30b0e2aae4ef3400134d2457cc8d580
Parth <parth.manocha2901@gmail.com>
2023-09-19 22:56:18 +0530

    fixess

--- test011.c
+++ test011.c
@@ -2,11 +2,5 @@
 
 int main()
 {
-    int perimeter, side;
-     printf("Enter the length of the side of the square \n ");
-    scanf("%d", & side);
-    perimeter = 4 * side; 
-    printf("Perimeter of the square: %d \n",perimeter);
-
     return 0;
 }
--- test1.c
+++ test1.c
@@ -2,25 +2,26 @@
 #include <conio.h>
 #include <math.h>
 
-/*
+
 #define PI 3.14f
 
-int main() 
+int main()
 {
-      int result,l,b,aot,h,x,y,z,perimeter,side,area,radius;
+      int result,l,b,aot,h,x,y,z,perimeter,side,area,radius,Volume,CSA;
 
     printf("Enter 0 if you have to find the area and peimeter of Square\n");
     printf("Enter 1 if you have to find the area and perimeter of rectangle\n");
     printf("Enter 2 if you have to find the area and perimeter of triangle\n");
     printf("Enter 3 if you have to find the area and perimeter of circle\n");
+    printf("Enter 4 if you have to find the CSA and Volume of sphere\n");
     printf("Please enter the number you want area and perimeter of \n");
     scanf("%d",&result);
     printf("You have entered %d as you want to find the area and perimeter of that shape \n" , result);
-    
+
     if(result==0) {
     printf("Enter the length of the side of the square \n ");
     scanf("%d", & side);
-    perimeter = 4 * side; 
+    perimeter = 4 * side;
     printf("Perimeter of the square:  %d\n",perimeter);
 
     area = side * side;
@@ -54,7 +55,18 @@ int main()
 
         area = PI * radius * radius;
         printf("Area of the Circle: %d\n", area);
-        
+
+    }
+    else if(result==4) {
+        printf("Enter the radius of sphere \n");
+        scanf("%d", &radius);
+        Volume = 4 * PI * radius * radius * radius / 3;
+        printf("Volume of the sphere: %d\n", Volume);
+
+        CSA = 4 * PI * radius * radius;
+        printf("Area of the sphere: %d\n", CSA);
+
+
     }
     else{
         printf("Select the valid number for output");
@@ -64,32 +76,64 @@ int main()
 
 
 }
-*/
 
-int main()
-{
-    int age;
-    printf("Enter your age\n");
-    scanf("%d", &age);
 
-    switch (age)
-    {
-    case 3:
-        printf("The age is 3");
-        break;
-    case 13:
-        printf("The age is 13");
-        break;
-    case 23:
-        printf("The age is 23");
-        break;
-    
-    default:
-    printf("Age is not 3,13 and 23");
-        break;
-    }
+// int main()
+// {
+//     int roll, marks;
+//     printf("Enter your roll no \n");
+//     scanf("%d", &roll);
 
-    return 0;
-}
+//     printf("Enter you marks\n");
+//     scanf("%d", &marks);
 
-    
+//     switch (roll)
+//     {
+//     case 3:
+//         printf(" Your roll no is included in the database of section 3\n ");
+//         switch (marks)
+//         {
+//             case 45:
+//             printf("And you have obtained 69 marks in total \n"); 
+//             break;
+
+
+//         default:
+//             printf("You have not obtained any marks\n");
+//         } 
+//         break;
+
+//     case 13:
+//         printf("Your roll no is included in the database of section 13\n ");
+//         switch(marks)
+//         {
+//             case 25:
+//             printf("And you have obtaned 75 marks in total");
+//             break;
+
+//         default:
+//             printf("You have not obtained any marks\n");
+//         }
+//         break;
+
+//     case 23:
+//         printf("Your roll no  is included in the database of section 23 ");
+//         switch(marks)
+//         {
+//             case 55:
+//             printf("And you have obtained 95 marks in total ");
+//             break;
+
+//          default:
+//             printf("You have not obtained any marks\n");
+//         }
+//         break;
+
+//     default:
+//         printf(" Your roll is incorrect pls check!");
+//         break;
+//     }
+
+//     return 0;}
+
+
