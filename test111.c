0d1242a6c1bbac16616ff7fa545de45fd8dbd66f
Parth <parth.manocha2901@gmail.com>
2023-10-01 23:34:45 +0530

    bore ho raha tha toh kuch banaleya

--- test111.c
+++ test111.c
@@ -1,51 +1,51 @@
 #include <stdio.h>
 
-// int returnMax(int array[], int n){
-//     int max = 0;
-//     for( int i=0; i < n ; i++)
-// {
-//     if (array[i] > max)
-//     {
-//         max = array[i];
-//     }
-// }
-// return max;
-// }
-
-// int main()
-// {
-
-//     int arr[] = {1, 2, 56, 45, 7, 8, 123, 556};
-//     int max = returnMax(arr, 8);
-//     printf("The maximun element in this array is %d", max);
-//     return 0;
-// }
-
-
-int main(){
-
-    int a,b,big;
-
-    printf("Enter two numbers to find which one is greater \n");
-    scanf("%d%d", &a,&b);
-
-  
-
-  
-
-
-     if(a>b){
-        big=a;
+int returnMax(int array[], int n){
+    int max = 0;
+    for( int i=0; i < n ; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
     }
-    else{
-       big=b;
-    }
-    printf("Biggest of the two number is :%d",big );
+}
+return max;
+}
 
+int main()
+{
 
-
-
+    int arr[] = {1, 2, 56, 45, 7, 8, 123, 556};
+    int max = returnMax(arr, 8);
+    printf("The maximun element in this array is %d", max);
     return 0;
+}
+
+
+// int main(){
+
+//     int a,b,big;
+
+//     printf("Enter two numbers to find which one is greater \n");
+//     scanf("%d%d", &a,&b);
+
+  
+
+  
+
+
+//      if(a>b){
+//         big=a;
+//     }
+//     else{
+//        big=b;
+//     }
+//     printf("Biggest of the two number is :%d",big );
+
+
+
+
+//     return 0;
 
     
-}
+// }
